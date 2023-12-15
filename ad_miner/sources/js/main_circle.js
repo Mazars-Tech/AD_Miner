3952bff05ca6bb23e352be863db36348b87784f7
VangelisHoareau <vangelis_hoareau@yahoo.fr>
2023-12-15 16:46:49 +0000

    feat: changing on prem background circle

--- ad_miner/sources/js/main_circle.js
+++ ad_miner/sources/js/main_circle.js
@@ -155,23 +155,23 @@ function display_all_hexagons(dico_entry) {
   var title_permissions = `<a data-bs-toggle="modal" href="#cardsModal" onclick=switchCards('permission')>
     <img src="../icons/main_circle/permissions.svg" class="title-section shadow" style="top:15%; left: 40%""/>
     </a>`;
-  $('.main_circle').append(title_permissions);
+  // $('.main_circle').append(title_permissions);
 
   var title_passwords = `<a data-bs-toggle="modal" href="#cardsModal" onclick=switchCards('passwords')>
         <img src="../icons/main_circle/passwords.svg" class="title-section shadow" style="top:62%; left: 10%""/>
     </a>`;
-  $('.main_circle').append(title_passwords);
+  // $('.main_circle').append(title_passwords);
 
   var title_kerberos = `<a data-bs-toggle="modal" href="#cardsModal" onclick=switchCards('kerberos')>
         <img src="../icons/main_circle/kerberos.svg" class="title-section shadow" style="top:62%; left: 70%;""/>
     </a>`;
-  $('.main_circle').append(title_kerberos);
+  // $('.main_circle').append(title_kerberos);
 
 
   var title_misc = `<a data-bs-toggle="modal" href="#cardsModal" onclick=switchCards('misc')>
         <img src="../icons/main_circle/misc.svg" class="title-section shadow" style="top:80%; left: 45%;""/>
     </a>`;
-  $('.main_circle').append(title_misc);
+  // $('.main_circle').append(title_misc);
 
 }
 
