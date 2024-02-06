15414f06adfb5e9181c73ec5d1ef3b121e56cb78
clement <clement.teytaud@gmail.com>
2024-02-06 15:42:22 +0000

    chore: add equality test for path_neo4j objects

--- ad_miner/sources/modules/path_neo4j.py
+++ ad_miner/sources/modules/path_neo4j.py
@@ -2,8 +2,19 @@ class Path:
     def __init__(self, nodes):
         self.nodes = nodes
 
+    def __eq__(self, other):
+        if not isinstance(other, Path):
+            return NotImplemented
+        if len(self.nodes) != len(other.nodes):
+            return False
+
+        ret = True
+        for i in range(len(self.nodes)):
+            ret = ret and (self.nodes[i] == other.nodes[i])
+        return ret
+
     def reverse(self):
         self.nodes.reverse()
         for i in range(len(self.nodes) - 1):
             self.nodes[i].relation_type = self.nodes[i + 1].relation_type
-        self.nodes[-1].relation_type = None
+        self.nodes[-1].relation_type = ""
